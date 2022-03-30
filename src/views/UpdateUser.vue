04f138a99bddcefc1e8643785021cbd451d8841f
dhanashrisdeshmukh <37360391+dhanashrisdeshmukh@users.noreply.github.com>
2022-03-30 20:42:10 +0530

    3:Update user
    User get updated in this component.

---
+++ src/views/UpdateUser.vue
@@ -0,0 +1,59 @@
+ 
+ <template>
+      <v-card >
+        <v-card-title>
+          <span class="headline">Edit user</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="user.name" label=" Name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="user.surname" label="surname"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="user.email" label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="user.phonenumber" label="Phone Number*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-btn  @click="updateUser()" required>Update User</v-btn>
+              </v-col>
+              <v-col cols="12">
+                <v-btn  @click="closeModal()" required>Cancel</v-btn>
+              </v-col>
+             
+              
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+       
+      </v-card>
+ </template>
+ <script>
+
+export default {
+  name: "User",
+  data() {
+    return {
+      userList: [],
+      user :this.$store.getters.getUser,
+    };
+  },
+
+ 
+  methods: {
+     
+   updateUser(){
+
+
+   }
+        
+  },
+
+}
+</script>
